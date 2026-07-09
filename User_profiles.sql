6808ab4c467311011b2c00083c5d1a17b7a77d7c
Oratile-Molokwane <molokwaneoratile@gmail.com>
2026-07-09 19:15:10 +0000

    Updating

--- User_profiles.sql
+++ User_profiles.sql
@@ -39,4 +39,19 @@ SELECT DISTINCT
         WHEN Race = ' ' THEN 'Unknown'
     ELSE Race
     END AS Race_Clean
-FROM user_profiles;        
+FROM user_profiles;    
+
+
+
+
+-- TELLING DATA BRICKS WHICH CATALOG(brighttv) AND SCHEMA(data) TO USE
+USE brighttv.data;
+
+-- select all columns to see the structure of the dataset
+SELECT *
+FROM brighttv.data.viewership_details;
+
+--DATE FUNCTIONS--
+--TO_DATE(name of the column you're converting)
+SELECT RecordDate2, TO_DATE(RecordDate2) AS Watch_Date -- TO_DATE() Extracts date from a Time Stamp
+FROM brighttv.data.viewership_details;
