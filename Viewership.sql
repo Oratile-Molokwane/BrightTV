159ca28b0bdb23c24ad624b84b7314c46c48603a
Oratile-Molokwane <molokwaneoratile@gmail.com>
2026-07-10 09:59:45 +0000

    Viewership

--- Viewership.sql
+++ Viewership.sql
@@ -10,3 +10,12 @@ FROM brighttv.data.viewership_details;
 --TO_DATE(name of the column you're converting)
 SELECT RecordDate2, TO_DATE(RecordDate2) AS Watch_Date -- TO_DATE() Extracts date from a Time Stamp
 FROM brighttv.data.viewership_details;
+
+SELECT UserID0,
+        RecordDate2,
+        TO_DATE(RecordDate2) AS watch_date,
+        DAYNAME(TO_DATE(RecordDate2)) AS Day_name,
+        MONTHNAME(TO_DATE(RecordDate2)) AS Month_name,
+        YEAR(TO_DATE(RecordDate2)) AS Event_year,
+        DAY(TO_DATE(RecordDate2)) AS Event_day
+FROM brighttv.data.viewership_details;      
